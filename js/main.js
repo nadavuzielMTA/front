1dec699deca09a2bca1bf102a97465f8ba523302
nadavu <nadavu@guardicore.com>
2021-09-26 16:36:59 +0300

    add login handle

--- js/main.js
+++ js/main.js
@@ -386,14 +386,27 @@ function documentReadyInit() {
 	var $form = jQuery(this);
 	jQuery(".register").on('click', function (e) {
 		jQuery.ajax({
+			type: 'POST',
 			url: '/api/login',
 			data: 'username=' + document.getElementById('register_user_name').value + '&password=' + document.getElementById('register_password').value,
 			success: function (msg) {
+				console.log(msg);
 				$form.find('.response').html(msg);
 			}
 		});
 	});
 
+	jQuery(".login").on('click', function (e) {
+		jQuery.ajax({
+			type: 'GET',
+			url: '/api/login',
+			data: 'username=' + document.getElementById('register_user_name').value + '&password=' + document.getElementById('register_password').value,
+			success: function (msg) {
+				console.log(msg);
+				$form.find('.response').html(msg);
+			}
+		});
+	});
 
 	
 
